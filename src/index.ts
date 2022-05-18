587a1bba16d255fdfd1fdb1eae17455be128c64a
edolganov <evgenij.dolganov@gmail.com>
2022-05-18 17:35:45 +0300

    Donation button export

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,12 @@ import {makeElem, makeStyleElement, postForm} from './util';
 import {initOpenSansFont} from './util/font';
 import {disableButton} from './button';
 import {SmartyPayButton, SmartyPayButtonProps} from './SmartyPayButton';
+import {SmartyPayDonation, SmartyPayDonationProps} from './SmartyPayDonation';
 import {labelPay, makeErrorParam, errorParam, tokenLabel} from './i18n';
 
 export {SmartyPayButton, SmartyPayButtonProps};
+export {SmartyPayDonation, SmartyPayDonationProps};
+
 export {Lang, parseLang, initOpenSansFont};
 
 export const I18n = {
