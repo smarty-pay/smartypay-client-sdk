905e470efc3179e8b534f22c0dfa49ae5abb776a
edolganov <evgenij.dolganov@gmail.com>
2022-05-20 16:29:27 +0300

    Update Theme visiability

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
  * @author Evgeny Dolganov <evgenij.dolganov@gmail.com>
  */
 import {Lang, parseLang} from "./model/lang";
+import {Theme} from "./model/theme";
 import {makeElem, makeStyleElement, postForm} from './util';
 import {initOpenSansFont} from './util/font';
 import {disableButton} from './button';
@@ -13,7 +14,7 @@ import {labelPay, makeErrorParam, errorParam, tokenLabel} from './i18n';
 export {SmartyPayButton, SmartyPayButtonProps};
 export {SmartyPayDonation, SmartyPayDonationProps};
 
-export {Lang, parseLang, initOpenSansFont};
+export {Theme, Lang, parseLang, initOpenSansFont};
 
 export const I18n = {
   labelPay,
